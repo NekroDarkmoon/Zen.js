f3a449a8dc3136cb293d7eb6546f397dcf574d83
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2021-11-09 14:40:49 -0800

    Finished getxp and working on givexp

--- main/commands/level.js
+++ main/commands/level.js
@@ -44,7 +44,7 @@ export default class Levels {
       )
       .addSubcommand( subcommand =>
         subcommand
-          .setName('levelboard')
+          .setName('xpboard')
           .setDescription('Display the leveling board for the server.')
           .addIntegerOption( option => option.setName('page').setDescription('Selected page to view.'))
       )
@@ -60,13 +60,114 @@ export default class Levels {
     const bot = interaction.client;
     if (!this.bot) this.bot = bot;
 
+    // Defer Reply
+    await interaction.deferReply();
     // Execute based on subcommand
     const sub = interaction.options.getSubcommand();
-    if (sub === "get") await this.getRep(interaction);
-    else if (sub === "giverep") await this.giveRep(interaction);
-    else if (sub === 'repboard') await this.repBoard(interaction, args);
-    else if (sub === 'setrep') await this.setRep(interaction);
+    if (sub === "get") await this.getLevel(interaction);
+    else if (sub === "givexp") await this.giveXp(interaction);
+    else if (sub === 'xpboard') await this.xpBoard(interaction, args);
+    else if (sub === 'setxp') await this.setXp(interaction);
 
     return;
+  }
+
+
+  /**
+   * 
+   * @param {Interaction} interaction 
+   */
+  async getLevel ( interaction ) {
+    let user = interaction.options.getUser('target');
+    if ( !user ) user = interaction.user;
+    
+    // Get Data from db
+    try {
+      // Get Level Information
+      const sql = 'SELECT * FROM xp WHERE server_id=$1 AND user_id=$2'
+      const vals = [interaction.guild.id, user.id];
+      const res = await this.bot.db.fetchOne(sql, vals);
+      // Get Message Information
+      const sql2 = 'SELECT * FROM logger WHERE server_id=$1 AND user_id=$2'
+      const vals2 = [interaction.guild.id, user.id];
+      const res2 = await this.bot.db.fetchOne(sql2, vals2);
+      // Message Builder
+      const xp = res ? res.xp : 0;
+      const level = res ? res.level : 0;
+      const nXp = this.levelUp(level);
+      const missXp = nXp - xp;
+      const msgCount = res2 ? res2.mes_count : 0;
+      // Message
+      let msg = `You are level ${level}, with ${xp} xp.\n`;
+      msg += `Level ${level+1} requires a total of ${nXp} xp: You need ${missXp} more.`
+
+      const e = new MessageEmbed()
+        .setThumbnail(user.avatarURL())
+        .setTitle(user.username)
+        .setDescription(msg)
+        .addField("XP", `${xp}/${nXp}`, true)
+        .addField("Level", `${level}`, true)
+        .addField("Messages", `${msgCount}`, true)
+
+      await interaction.editReply({embeds: [e]});
+
+    } catch ( e ) {console.error(e); return;}
+  }
+
+
+  /**
+   * 
+   * @param {Interaction} interaction 
+   */
+  async giveXp ( interaction ) {
+    // Data builder
+    const member = interaction.member;
+    const user = interaction.options.getUser('target');
+    const xp = interaction.options.getInteger('amount');
+    // Validation - Admin
+    if (!member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      const msg = `Error: Permissions not ment. \`Administrator\` `;
+      await interaction.editReply(msg);
+    }
+
+    // Get Existing
+    let existing = null;
+    try {
+      const sql = 'SELECT * FROM xp WHERE server_id=$1 AND user_id=$2';
+      const vals = [interaction.guild.id, user.id];
+      existing = await this.bot.db.fetchOne(sql, vals); 
+    } catch ( e ) {
+      console.error(e);
+      await interaction.editReply("Error: Unable to complete.")
+    } 
+    // Calculate Updated details
+    const fXp = existing ? existing[2] + xp : xp;
+    const level = existing ? existing[3] : 0;
+
+
+  }
+
+  async setXp ( interaction ) {}
+  async xpBoard ( interaction, args ) {}
+
+
+  /**
+   * 
+   * @param {Number} level 
+   * @returns {Number} xp
+   */
+  levelUp ( level ) {
+    const baseXp = 400;
+    const inc = 200;
+    return Math.floor((baseXp * level) + (inc * level * (level - 1 ) * 0.5 ));
+  }
+
+
+  /**
+   * 
+   * @param {*} xp 
+   */
+  calcLevel ( xp ) {
+      
   }
 }
